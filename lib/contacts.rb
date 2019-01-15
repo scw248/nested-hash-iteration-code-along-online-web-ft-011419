6464f3e8e350f9b48beab831bbd6463ca71a06b5
Stephen Wright <scw248@gmail.com>
2019-01-15 14:44:07 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, details|
     details.each do |attribute, data|
-      if attribute == favorite_ice_cream_flavors
+      if attribute == :favorite_ice_cream_flavors
         data.delete_if { |ice_cream| ice_cream == "strawberry" }
     end
   end
